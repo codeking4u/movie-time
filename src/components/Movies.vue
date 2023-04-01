f4cd7c8545a9e87b83c596b606cf27e6abe61c91
amar.singh <amar.singh@kuehne-nagel.com>
2023-04-01 18:51:00 +0300

    remove boilerplate code and components + access TVmaze api setup

---
+++ src/components/Movies.vue
@@ -0,0 +1,20 @@
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'Movies',
+  props: {
+    movies: []
+  }
+})
+</script>
+
+<template>
+  <div>
+    <ul>
+      <li v-for="m in movies" :key="m.id">{{ m.name }}</li>
+    </ul>
+  </div>
+</template>
+
+<style scoped></style>
